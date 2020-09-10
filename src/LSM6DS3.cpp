cb12ca8492e1fbffd851a87871765a4f2c4cc143
D. Lowl <dlowl@sihvi.com>
2020-09-10 14:34:35 +0500

    Change accelerometre resolution 4G -> 16G
    TODO: parameterise in begin()

--- src/LSM6DS3.cpp
+++ src/LSM6DS3.cpp
@@ -84,9 +84,9 @@ int LSM6DS3Class::begin()
   //set the gyroscope control register to work at 104 Hz, 2000 dps and in bypass mode
   writeRegister(LSM6DS3_CTRL2_G, 0x4C);
 
-  // Set the Accelerometer control register to work at 104 Hz, 4G,and in bypass mode and enable ODR/4
+  // Set the Accelerometer control register to work at 104 Hz, 16G,and in bypass mode and enable ODR/4
   // low pass filter(check figure9 of LSM6DS3's datasheet)
-  writeRegister(LSM6DS3_CTRL1_XL, 0x4A);
+  writeRegister(LSM6DS3_CTRL1_XL, 0x47);
 
   // set gyroscope power mode to high performance and bandwidth to 16 MHz
   writeRegister(LSM6DS3_CTRL7_G, 0x00);
@@ -122,9 +122,9 @@ int LSM6DS3Class::readAcceleration(float& x, float& y, float& z)
     return 0;
   }
 
-  x = data[0] * 4.0 / 32768.0;
-  y = data[1] * 4.0 / 32768.0;
-  z = data[2] * 4.0 / 32768.0;
+  x = data[0] * 16.0 / 32768.0;
+  y = data[1] * 16.0 / 32768.0;
+  z = data[2] * 16.0 / 32768.0;
 
   return 1;
 }
